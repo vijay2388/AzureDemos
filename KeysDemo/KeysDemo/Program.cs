a42497eb772e1c4f0070b41d728297d6da130c47
Vijay Vishwakarma <vijay2388@gmail.com>
2021-08-19 15:09:29 +0000

    Initial Commit

---
+++ KeysDemo/KeysDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Azure.Identity;
+using Azure.Security.KeyVault.Keys;
+
+namespace KeysDemo
+{
+    /// <summary>
+    /// https://docs.microsoft.com/en-us/azure/key-vault/keys/quick-create-net?tabs=azure-cli
+    /// </summary>
+    class Program
+    {
+        const string _vaultName = "mykeyvault007999";
+        const string _keyName = "key02";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Working with Azure Key Vault Key API...");
+
+            var kvUri = "https://" + _vaultName + ".vault.azure.net";
+
+            var client = new KeyClient(new Uri(kvUri), new DefaultAzureCredential());
+
+            var key = client.GetKeyAsync(_keyName).Result;
+
+            Console.WriteLine($"[mydiskencryptionkey] value is {key.Value.KeyType}");
+
+            Console.ReadLine();
+        }
+    }
+}
